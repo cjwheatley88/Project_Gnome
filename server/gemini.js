d7baa26b5413e43e338a467a5a9bf94198e8488d
Chris W <cjwheatley88@gmail.com>
2025-01-31 14:44:23 +1100

    server.js upated, removed console.log troubleshooting lines

--- server/gemini.js
+++ server/gemini.js
@@ -8,13 +8,10 @@ const genAI = new GoogleGenerativeAI(APIKEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function generateContent(prompt) {
-  //console.log('Received prompt:', prompt); // Log the received prompt
-  //console.log('Using API Key:', APIKEY); // Log the API key
-
   try {
     const result = await model.generateContent(prompt);
     const content = await result.response.text(); // Ensure the response is awaited
-    console.log('Generated content:', content); // Log the API response
+    //console.log('Generated content:', content);
     return content;
   } catch (error) {
     console.error("Error generating content:", error);
